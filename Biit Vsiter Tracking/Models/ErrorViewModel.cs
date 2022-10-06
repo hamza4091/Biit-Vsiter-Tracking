ac6e061dbf7f3c23589c56118bfe3e470b76aa87
technifi <technifi@DESKTOP-P7LDE5U>
2022-10-07 00:02:17 +0500

    Add project files.

---
+++ Biit Vsiter Tracking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Biit_Vsiter_Tracking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
